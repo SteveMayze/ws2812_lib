b805b5c41bdd7d7bf279af2c2138ca88ff99aead
Steve Mayze <smayze@yahoo.com##>
2025-02-02 18:05:47 +0100

    Initial commit

---
+++ library.c
@@ -0,0 +1,16 @@
+/*
+ * ws2812_lib.c
+ *
+ * Created: 02/02/2025 17:57:57
+ * Author : Steven
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
